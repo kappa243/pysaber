1a8d6a4f1bbce27047a1f9a75fb3c25a261408e3
Dawid Kopec <kopecd@student.agh.edu.pl>
2024-06-04 19:44:09 +0200

    added test_indcpa_kem_enc() function

--- pysaber/poly.py
+++ pysaber/poly.py
@@ -12,6 +12,17 @@ from pysaber.saber_params import (SABER_L, SABER_N, SABER_POLYCOINBYTES,
 
 
 def gen_matrix(A, seed):
+    """
+    Generate a matrix A using a given seed. The matrix A is populated with polynomial vectors.
+
+    Parameters:
+    A (list): The matrix to be generated, which is a list of polynomial vectors.
+    seed (bytes): The seed used for generating the matrix.
+
+    The method uses the SHAKE-128 hash function to generate a digest based on the seed. 
+    This digest is then divided into chunks, each of which is converted into a polynomial vector 
+    and stored in the corresponding position of the matrix A.
+    """
     buf = hashlib.shake_128(seed).digest(SABER_L * SABER_POLYVECBYTES)
 
     for i in range(SABER_L):
@@ -19,6 +30,18 @@ def gen_matrix(A, seed):
 
 
 def gen_secret(s, seed):
+    """
+    Generate a secret vector s using a given seed. The secret vector is populated with polynomials.
+
+    Parameters:
+    s (list): The secret vector to be generated, which is a list of polynomials.
+    seed (bytes): The seed used for generating the secret vector.
+
+    The method uses the SHAKE-128 hash function to generate a digest based on the seed. 
+    This digest is then divided into chunks, each of which is used to generate a polynomial 
+    through a central binomial distribution (CBD) function and stored in the corresponding 
+    position of the secret vector s.
+    """
     buf = hashlib.shake_128(seed).digest(SABER_L * SABER_POLYCOINBYTES)
 
     for i in range(SABER_L):
@@ -43,3 +66,37 @@ def matrix_mul(A, s, res, transpose):
                 poly_mul_acc(A[j][i], s[j], res[i])
             else:
                 poly_mul_acc(A[i][j], s[j], res[i])
+
+
+# For further context on method inner_prod reffer to section 8.3.9 in documentation
+def inner_prod(b, s, res):
+  """
+  This function computes the inner product of two matrices b and s,
+  and stores the result in the vector res.
+
+  Args:
+      b: A list of lists representing a matrix of size SABER_L x SABER_N.
+      s: A list of lists representing a matrix of size SABER_L x SABER_N.
+      res: A list to store the inner product result.
+
+  Returns:
+      None
+  """
+  for j in range(SABER_L):
+    poly_mul_acc(b[j], s[j], res)
+
+
+def shift_left(pin, shift):
+    pout = np.zeros(SABER_N, dtype=np.uint16)
+    for i in range(SABER_N - 1):
+        pout[i] = (pin[i] << shift) | (pin[i + 1] >> (8 - shift))
+    pout[SABER_N - 1] = pin[SABER_N - 1] << shift
+    return pout
+
+
+def shift_right(pin, shift):
+    pout = np.zeros(SABER_N, dtype=np.uint16)
+    for i in range(SABER_N - 1, 0, -1):
+        pout[i] = (pin[i] >> shift) | (pin[i - 1] << (8 - shift))
+    pout[0] = pin[0] >> shift
+    return pout
